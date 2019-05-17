89569b384a1dc619063ec51cee643217b8c07dde
jovi wang <uwings@qq.com>
2019-05-17 22:47:19 +0800

    modify push.sh

--- _book/push.sh
+++ _book/push.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
-gitbook build &&
-git ls-files  --modified --others --exclude=node_modules/  --exclude=.DS_Store | xargs git add &&
-git commit -m"$1" &&
-git push origin &&
+gitbook build
+git ls-files  --modified --others --exclude=node_modules/  --exclude=.DS_Store | xargs git add
+git commit -m"$1"
+git push origin
 # guidelines.cc remote url name
 echo "push to guidelines complete."
-git push gitee &&
+git push gitee
 # gitee remote url name
 echo "push to gitee complete."
-git push coding &&
+git push coding
 # coding remote url name
 echo "push to coding complete."
